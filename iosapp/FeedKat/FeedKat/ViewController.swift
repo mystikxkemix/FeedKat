7fe53f5e6452d538aa8618f09f3a6483312ab283
Mike Oliva <mike.oliva.pro@gmail.com>
2016-09-13 10:42:08 +0200

    First page schlague

--- iosapp/FeedKat/FeedKat/ViewController.swift
+++ iosapp/FeedKat/FeedKat/ViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    //MARK : Properties
+    @IBOutlet var texttitle: UILabel!
+    @IBOutlet var edittextfield: UITextField!
+    @IBOutlet var savebutton: UIButton!
+    @IBOutlet var nametext: UILabel!
+
+    @IBAction func onsaveclick(sender: UIButton) {
+        nametext.text = "Name : \(edittextfield.text!)"
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
