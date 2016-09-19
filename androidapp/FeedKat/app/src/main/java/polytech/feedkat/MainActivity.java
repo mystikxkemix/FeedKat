fba2672a90307aa295aa09bbcbe31b539947ae85
Mike Oliva <mike.oliva.pro@gmail.com>
2016-09-19 16:49:20 +0200

    Clean app

--- androidapp/FeedKat/app/src/main/java/polytech/feedkat/MainActivity.java
+++ androidapp/FeedKat/app/src/main/java/polytech/feedkat/MainActivity.java
@@ -1,13 +1,25 @@
 package polytech.feedkat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private View mContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+    }
+
 }
