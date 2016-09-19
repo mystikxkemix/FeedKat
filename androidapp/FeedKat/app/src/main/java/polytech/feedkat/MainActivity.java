77dcbff1edd950cb11076f011243cebccd062fee
Olivier Lafon <olivierlafon@hotmail.fr>
2016-09-19 17:51:03 +0200

    pge connexion

--- androidapp/FeedKat/app/src/main/java/polytech/feedkat/MainActivity.java
+++ androidapp/FeedKat/app/src/main/java/polytech/feedkat/MainActivity.java
@@ -2,7 +2,11 @@ package polytech.feedkat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private View mContentView;
@@ -12,8 +16,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        FrameLayout main = (FrameLayout)findViewById(R.id.main_content);
 
+        TextView t = new TextView(this);
+        t.setText("FeedKat, pour une alimentation au poil");
+        t.setTextSize(20);
+        t.setGravity(View.TEXT_ALIGNMENT_CENTER);
 
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.TOP;
+        lp.setMargins(0,50,0,0);
+
+        t.setLayoutParams(lp);
+
+        main.addView(t);
     }
 
     @Override
