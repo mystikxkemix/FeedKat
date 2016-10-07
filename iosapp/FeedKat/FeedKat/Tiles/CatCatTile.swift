f9fbcd8d63e042f6d1e2d72878a2fb391d663234
Mike Oliva <mike.oliva.pro@gmail.com>
2016-10-07 17:01:05 +0200

    Change place files

---
+++ iosapp/FeedKat/FeedKat/Tiles/CatCatTile.swift
@@ -0,0 +1,22 @@
+//
+//  CatCatTile.swift
+//  FeedKat
+//
+//  Created by Mike OLIVA on 07/10/2016.
+//  Copyright © 2016 Mike OLIVA. All rights reserved.
+//
+
+import UIKit
+
+class CatcatTile: Tile
+{
+    init(Image: UIImage, Name:String)
+    {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
