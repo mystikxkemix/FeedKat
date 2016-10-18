c7790fe54d5947407c6015eadc265a3070f1baff
Mike Oliva <mike.oliva.pro@gmail.com>
2016-10-18 15:59:06 +0200

    begin API

---
+++ iosapp/FeedKat/FeedKat/FeedkatAPI.swift
@@ -0,0 +1,19 @@
+//
+//  FeedkatAPI.swift
+//  FeedKat
+//
+//  Created by Mike OLIVA on 18/10/2016.
+//  Copyright © 2016 Mike OLIVA. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+open class FeedKatAPI:NSObject
+{
+    fileprivate static var isLocal = false
+    fileprivate static var prodServerAddr = "http://feedkat.ddns.net:80/api/web/index.php"
+    fileprivate static var localServerAddr = "http://192.168.43.12:80/api/web/index.php"
+    
+    open static 
+}
