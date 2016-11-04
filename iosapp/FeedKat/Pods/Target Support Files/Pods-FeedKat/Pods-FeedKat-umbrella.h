7c9eb0ba60162c7d17f4c64e2cce84c44ac8fc92
Mike Oliva <mike.oliva.pro@gmail.com>
2016-11-04 14:54:20 +0100

    Init branch webapp

--- iosapp/FeedKat/Pods/Target Support Files/Pods-FeedKat/Pods-FeedKat-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_FeedKatVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_FeedKatVersionString[];
-
--- iosapp/FeedKat/Pods/Target Support Files/Pods-FeedKatUITests/Pods-FeedKatUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FeedKatUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FeedKatUITests
-@end
